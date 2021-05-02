ec4413f9300fc370b0c36f44914d2d369938bd6b
Philip <pilivpp4@gmailcom>
2021-05-03 00:41:55 +0200

    Add authentication

--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -1,18 +1,24 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using RestaurantAPI.Entieties;
 using RestaurantAPI.Middleware;
 using RestaurantAPI.Models;
+using RestaurantAPI.Models.Validators;
 using RestaurantAPI.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace RestaurantAPI
@@ -29,13 +35,38 @@ namespace RestaurantAPI
       // This method gets called by the runtime. Use this method to add services to the container.
       public void ConfigureServices(IServiceCollection services)
       {
-         services.AddControllers();
+         var authenticationSettings = new AuthenticationSettings();
+
+         Configuration.GetSection("Authentication").Bind(authenticationSettings);
+
+         services.AddSingleton(authenticationSettings);
+         services.AddAuthentication(option =>
+         {
+            option.DefaultAuthenticateScheme = "Bearer";
+            option.DefaultScheme = "Bearer";
+            option.DefaultChallengeScheme = "Bearer";
+         }).AddJwtBearer(cfg =>
+         {
+            cfg.RequireHttpsMetadata = false;
+            cfg.SaveToken = true;
+            cfg.TokenValidationParameters = new TokenValidationParameters
+            {
+               ValidIssuer = authenticationSettings.JwtIssuer,
+               ValidAudience = authenticationSettings.JwtIssuer,
+               IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
+            };
+         });
+
+         services.AddControllers().AddFluentValidation();
          services.AddDbContext<RestaurantDbContext>();
          services.AddScoped<RestaurantSeeder>();
          services.AddAutoMapper(this.GetType().Assembly);
          services.AddScoped<IRestaurantService, RestaurantService>();
          services.AddScoped<IDishService, DishService>();
+         services.AddScoped<IAccountService, AccountService>();
          services.AddScoped<ErrorHandlingMiddleware>();
+         services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+         services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
          services.AddScoped<RequestTimeMiddleware>();
          services.AddSwaggerGen();
       }
@@ -53,10 +84,11 @@ namespace RestaurantAPI
 
          app.UseMiddleware<ErrorHandlingMiddleware>();
          app.UseMiddleware<RequestTimeMiddleware>();
+         app.UseAuthentication();
          app.UseHttpsRedirection();
 
          app.UseSwagger();
-         app.UseSwaggerUI(c => 
+         app.UseSwaggerUI(c =>
          {
             c.SwaggerEndpoint("/swagger/v1/swagger.json", "Restaurant API");
          });
