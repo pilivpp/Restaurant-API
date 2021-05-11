10e5b8106e2c106b07549ffbc061c95bda965b07
Philip <pilivpp4@gmailcom>
2021-05-11 18:51:09 +0200

    Add files for download. Public and private with authorization

---
+++ RestaurantAPI/Controllers/FileController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+   [Route("file")]
+   [Authorize]
+   public class FileController : ControllerBase
+   {
+      public ActionResult GetFile([FromQuery] string fileName)
+      {
+         var rootPath = Directory.GetCurrentDirectory();
+
+         var filePath = $"{rootPath}/PrivateFiles/{fileName}";
+
+         var fileExist = System.IO.File.Exists(filePath);
+
+         if(!fileExist)
+         {
+            return NotFound();
+         }
+
+         var fileContents = System.IO.File.ReadAllBytes(filePath);
+
+         var contentProvider = new FileExtensionContentTypeProvider();
+         contentProvider.TryGetContentType(filePath, out string contentType);
+
+         return File(fileContents, contentType, fileName);
+      }
+   }
+}
--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -95,6 +95,7 @@ namespace RestaurantAPI
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
       public void Configure(IApplicationBuilder app, IWebHostEnvironment env, RestaurantSeeder seeder)
       {
+         app.UseStaticFiles();
          app.UseCors("FrontendClient");
          seeder.Seed();
 
